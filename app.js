5ac894fdb5fa2b8dd0f97ea2f1a9072963afcf15
TuLeHuynhNhat <tulehuynhnhat05@gmail.com>
2023-03-16 16:03:12 +0700

    add header middleware 3

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const AppError = require('./utils/appError');
 
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://digital-camera-server-demo.onrender.com');
+  res.setHeader('Access-Control-Allow-Origin', '*');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
